0f8b4a299767080307a5a92261ca764d1c5206a7
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-11 20:53:53 -0500

    Correo de activación

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class UserProfile(AbstractUser):
-    username = models.CharField(max_length=15, unique=True)
+    username = models.CharField(max_length=15, unique=True, verbose_name="Nombre de Usuario")
     first_name = models.CharField(max_length=100, default='', verbose_name="Nombres")
     last_name = models.CharField(max_length=100, default='', verbose_name="Apellidos")
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True, verbose_name="Activo")
     token = models.CharField(blank=True, max_length=50)
-    email = models.EmailField(unique=True,blank=True, null=True)
+    email = models.EmailField(unique=True,blank=True, null=True,verbose_name="Correo electrónico")
     email_is_valid = models.BooleanField(default=False)
     date_joined = models.DateTimeField(blank=True)
     identification = models.CharField(db_index=True, unique=True, max_length=15, verbose_name="Cédula")
@@ -18,17 +18,42 @@ class UserProfile(AbstractUser):
     phone_indicator = models.CharField(max_length=4, default='', verbose_name="Indicador")
     country = models.CharField(max_length=100, default='', verbose_name="País")
     state = models.CharField(max_length=100, default='', verbose_name="Departamento")
-    company = models.ForeignKey('companies.Company',on_delete=models.CASCADE,verbose_name="Compañia",null=True,blank=False)
+    company = models.ForeignKey('companies.Company',on_delete=models.CASCADE,verbose_name="Compañia",null=False,blank=False)
     city = models.CharField(max_length=100, default='', verbose_name="Ciudad")
-
-    #USERNAME_FIELD = 'identification'
-    #REQUIRED_FIELDS = ['password','email']
 
     def get_full_name(self):
         full_name = ''
         full_name += str(self.first_name) if self.first_name else ''
         full_name += " " + str(self.last_name) if self.last_name else ''
         return full_name
+
+    def send_activation_email(self):
+        from django.core.mail import EmailMultiAlternatives
+        from django.conf import settings
+        token_generator = PasswordResetTokenGenerator()
+        token = token_generator.make_token(self)
+        url = "{0}validate_email?ut={1}&token={2}".format(settings.SITE_URL,self.id, token)
+        html = """
+            <html>
+                <p>Estimado usuario, el registro de su cuenta fue exitoso.</p>
+                <p>Por favor active su cuenta haciendo click <a target="_blank" href="{0}">aquí</a>.</p>
+                <br>
+                <p>Si por algun motivo su dispositivo no abre la ventana, copie y pegue en su navegador el siguiente link</p>
+                <p>{0}</p>
+            </html>
+        """.format(url)
+        send_mail(
+            "ACTIVA TU CUENTA",
+            '',
+            settings.EMAIL_HOST_USER,
+            [self.email],
+            html_message=html,
+            fail_silently=False
+        )
+
+    def create(self,args):
+        return super.create(args)
+
 
     def __str__(self) -> str:
         return self.get_full_name()
--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,14 +1,51 @@
+from django.utils import timezone
 from apps.accounts.models import UserProfile
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+import threading
 
-
+from apps.companies.models import Company
 class UserProfileSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True,max_length=100)
-    password = serializers.CharField(required=True, min_length=6,max_length=20,write_only=True)
-    
-    # def validate_password(self):
-    #     return make_password(self.cleaned_data['password'])
+    email = serializers.EmailField(required=True,
+                                    max_length=100,
+                                    validators=[UniqueValidator(queryset=UserProfile.objects.all())])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    identification = serializers.IntegerField(required=True,validators=[UniqueValidator(queryset=UserProfile.objects.all())])
+    username = serializers.CharField(read_only=True,)
+    company = serializers.IntegerField()
     class Meta:
         model = UserProfile
-        fields = ['identification', 'email','password']
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name','identification','company')
+        extra_kwargs = {
+            'identification': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'company':{'required':True}
+        }
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Contraseñas ingresadas no coinciden"})
+
+        return attrs
+
+    def create(self, validated_data):
+        print(validated_data)
+        company = Company.objects.get(id=self.initial_data['company'])
+        now = timezone.now()
+        user = UserProfile.objects.create(
+            username=str(validated_data['identification']),
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            identification=validated_data['identification'],
+            company=company,
+            date_joined=now,
+            is_staff=True,
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
