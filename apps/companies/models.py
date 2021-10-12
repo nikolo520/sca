38c6ad711cfa919fee56ddb7943a1ce090d1fda7
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-11 20:54:08 -0500

    Ajustando Company

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -1,20 +1,22 @@
+from typing import ClassVar
 from django.db import models
 from django.db.models.base import Model
+from rest_framework.validators import UniqueValidator
 
 class Company(Model):
     id = models.AutoField(primary_key=True)
     business_name = models.CharField(max_length=100, default='', verbose_name="Nombre de la empresa")
     trade_name = models.CharField(max_length=100, default='', verbose_name="Nombre comercial")
-    is_active = models.BooleanField(default=True)
-    email = models.EmailField(blank=True, null=True)
+    is_active = models.BooleanField(default=True, verbose_name="Activo")
+    email = models.EmailField(blank=True, null=True, verbose_name="Correo eletrónico")
     email_is_valid = models.BooleanField(default=False)
     nit = models.CharField(db_index=True, unique=True, max_length=15, verbose_name="Nit")
-    phone = models.CharField(max_length=10, default='', verbose_name="Teléfono")
-    phone_indicator = models.CharField(max_length=4, default='', verbose_name="Indicador")
-    country = models.CharField(max_length=100, default='', verbose_name="País")
-    state = models.CharField(max_length=100, default='', verbose_name="Departamento")
-    city = models.CharField(max_length=100, default='', verbose_name="Ciudad")
-    web_page = models.CharField(max_length=100, default='', verbose_name="Página Web")
+    phone = models.CharField(max_length=10, verbose_name="Teléfono",null=True, blank=True)
+    phone_indicator = models.CharField(max_length=4, default='57', verbose_name="Indicador",null=True, blank=True)
+    country = models.CharField(max_length=100, verbose_name="País", null=True, blank=True)
+    state = models.CharField(max_length=100, verbose_name="Departamento", null=True, blank=True)
+    city = models.CharField(max_length=100, verbose_name="Ciudad", null=True, blank=True)
+    web_page = models.CharField(max_length=100, verbose_name="Página Web",help_text="https://...",null=True, blank=True)
 
     def __str__(self) -> str:
         return self.business_name if self.business_name else 'No definido'
--- apps/companies/serializers.py
+++ apps/companies/serializers.py
@@ -1,18 +1,13 @@
-from enum import unique
-
-from django.db.models.fields import AutoField
-from rest_framework.fields import ReadOnlyField
 from apps.companies.models import Company
+from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 
 class CompanySerializer(serializers.Serializer):
-
     id = serializers.IntegerField(read_only=True)
-    nit = serializers.IntegerField(required=True)
+    nit = serializers.CharField(required=True)
     business_name = serializers.CharField(required=True, max_length=100)
     trade_name = serializers.CharField(required=True, max_length=100)
     email = serializers.EmailField(max_length=50)
-    email_is_valid = serializers.BooleanField(default=False,read_only=True)
     phone = serializers.CharField(max_length=10)
     phone_indicator = serializers.CharField(max_length=4, default='57')
     country = serializers.CharField(max_length=100,)
@@ -23,14 +18,32 @@ class CompanySerializer(serializers.Serializer):
     
     class Meta:
         model = Company
-        fields = ('id', 
-                    'business_name',
-                    'is_active',
-                    'email',
-                    'email_is_valid',
-                    'phone',
-                    'phone_indicator',
-                    'country',
-                    'state',
-                    'city',
-                    )
+        fields = (
+            'id',
+            'nit',
+            'business_name',
+            'trade_name',
+            'email',
+            'phone',
+            'phone_indicator',
+            'country',
+            'state',
+            'city',
+            'web_page',
+            'is_active',
+        )
+
+    def create(self, validated_data):
+        company = Company.objects.create(
+            nit = validated_data['nit'],
+            business_name = validated_data['business_name'],
+            trade_name = validated_data['trade_name'],
+            phone = validated_data['phone'],
+            phone_indicator = validated_data['phone_indicator'],
+            country = validated_data['country'],
+            state = validated_data['state'],
+            city = validated_data['city'],
+            web_page = validated_data['web_page']
+        )
+        company.save()
+        return company
