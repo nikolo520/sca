aed71c535bf31278a87b7251e461fcbdff718639
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-11 23:31:14 -0500

    AccessPoint views y urls

--- apps/access_points/views.py
+++ apps/access_points/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views import generic
+from apps.access_points.models import AccessPoint
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseNotAllowed
+class AccessPointListView(LoginRequiredMixin,generic.ListView):
+    def get(self, request, *args, **kwargs):
+        if request.user:
+            if request.user.id:
+                self.model = AccessPoint
+                self.queryset = AccessPoint.objects.filter(company=request.user.company)
+                self.redirect_field_name = 'redirect_to'
+                return super().get(self, request, *args, **kwargs)
+        return HttpResponseNotAllowed()
--- sca/urls.py
+++ sca/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path,include
 from api.views import Login,Logout,validate_email,obtain_auth_token
+from apps.accounts import views
+from apps.access_points.views import AccessPointListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/1.0/',include(('api.urls','api'))),
     path('api_generate_token/', obtain_auth_token.as_view()),
     path('validate_email', validate_email),
-    path('login/',Login.as_view(), name = 'login'),
-    path('logout/', Logout.as_view()),
+    path('login/', Login.as_view(), name = 'login'),
+    path('logout/', Logout.as_view(), name = 'logout'),
+    path('', AccessPointListView.as_view(),name='list_access_points'),
+    path('access_points/',include(('apps.access_points.urls','access_points'))),
+    #path('accounts/',include(('apps.accounts.urls','accounts'))),
+    #path('companies/',include(('apps.companies.urls','companies'))),
 ]
