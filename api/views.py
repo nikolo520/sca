0b5d351b821a0adb9564b2a707d535437b0ccd8b
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-12 00:29:48 -0500

    Personalizando el Login

--- api/views.py
+++ api/views.py
@@ -16,14 +16,7 @@ from apps.companies.serializers import CompanySerializer
 from apps.accounts.serializers import UserProfileSerializer
 from rest_framework import permissions
 from rest_framework import generics
-from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import never_cache
-from django.views.decorators.csrf import csrf_protect
-from django.views.generic.edit import FormView
-from django.contrib.auth import login,logout,authenticate, tokens
-from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout, tokens
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
@@ -86,26 +79,6 @@ class AccessPointsListCreate(generics.ListCreateAPIView):
         self.authentication_class = (TokenAuthentication,)
         kwargs['company'] = company_current_user.id
         return super().post(request, *args, **kwargs)
-class Login(FormView):
-    template_name = "login.html"
-    form_class = AuthenticationForm
-    success_url = reverse_lazy('api:list_access_points')
-
-    @method_decorator(csrf_protect)
-    @method_decorator(never_cache)
-    def dispatch(self,request,*args,**kwargs):
-        if request.user.is_authenticated:
-            return HttpResponseRedirect(self.get_success_url())
-        else:
-            return super(Login,self).dispatch(request,*args,*kwargs)
-
-    def form_valid(self,form):
-        user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password'])
-        token,_ = Token.objects.get_or_create(user = user)
-        if token:
-            login(self.request, form.get_user())
-            return super(Login,self).form_valid(form)
-
 class Logout(APIView):
     def get(self,request, format = None):
         request.user.auth_token.delete()
--- sca/urls.py
+++ sca/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path,include
-from api.views import Login,Logout,validate_email,obtain_auth_token
+from api.views import validate_email,obtain_auth_token
 from apps.accounts import views
 from apps.access_points.views import AccessPointListView
-
+from apps.accounts.views import Login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/1.0/',include(('api.urls','api'))),
     path('api_generate_token/', obtain_auth_token.as_view()),
     path('validate_email', validate_email),
+    path('', include('django.contrib.auth.urls')),
     path('login/', Login.as_view(), name = 'login'),
-    path('logout/', Logout.as_view(), name = 'logout'),
+    #path('logout/', Logout.as_view(), name = 'logout'),
     path('', AccessPointListView.as_view(),name='list_access_points'),
     path('access_points/',include(('apps.access_points.urls','access_points'))),
     #path('accounts/',include(('apps.accounts.urls','accounts'))),
