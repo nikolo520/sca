e95a3d47db002af631a503820f0acf1bedf1f7d2
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-12 11:57:11 -0500

    Templates password

--- sca/settings.py
+++ sca/settings.py
@@ -9,14 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
 from pathlib import Path
 from .config import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Application definition
 
